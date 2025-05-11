117a0f2ceaf013956c2ace7f547e81924e4c6fae
Beatriz Firmado <firmadobeatriz@gmail.com>
2025-05-10 22:44:41 -0300

    resolução do exercício 1011 - esfera

---
+++ pages/esfera/esfera.js
@@ -0,0 +1,12 @@
+function volumeEsfera(raio) {
+    let pi = 3.14159
+    return (4.0 / 3) * pi * Math.pow(raio, 3)
+}
+
+let esfera1 = volumeEsfera(3)
+let esfera2 = volumeEsfera(15)
+let esfera3 = volumeEsfera(1523)
+
+console.log(`VOLUME 1 = ${esfera1.toFixed(3)}`)
+console.log(`VOLUME 2 = ${esfera2.toFixed(3)}`)
+console.log(`VOLUME 3 = ${esfera3.toFixed(3)}`)
